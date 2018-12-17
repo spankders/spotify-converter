3b702e5f318ec956688c86c8e18df10bd0910345
Ardan Ardzz <reyardn@gmail.com>
2018-12-17 20:22:31 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Isi email dengan email akun spotify kamu
+Isi password dengan password akun spotify kamu
+Belum ada akun? silahkan daftar dulu :)
+Di https://www.spotify.com/id/signup/
+*/
+$email = ""; // email spotify
+$password = ""; // password dari email spotify
+?>
