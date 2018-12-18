a77ad6a721a3ef2e0e55c65da325e1a25ba70aa8
Ardan Ardzz <reyardn@gmail.com>
2018-12-19 05:03:45 +0700

    Update class.spotify.php

--- class.spotify.php
+++ class.spotify.php
@@ -93,7 +93,15 @@ class spotify{
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_HEADER, 1);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-		'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36', // user-agent bisa diganti sesuka hati <3
+			"Host: accounts.spotify.com",
+			"User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0",
+			"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+			"Accept-Language: en-US,en;q=0.5",
+			"Accept-Encoding: gzip, deflate",
+			"Referer: https://www.spotify.com/",
+			"DNT: 1",
+			"Connection: close",
+			"Upgrade-Insecure-Requests: 1",
 		));
 		$res = curl_exec($ch);
 		$header = substr($res, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
